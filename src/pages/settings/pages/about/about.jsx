66a9a18cbb59d43b1e47f6ad762f53f43fe31fb7
Blake Kaufman <blakekaufman9@gmail.com>
2025-10-03 10:25:28 -0400

    bumping app version

--- src/pages/settings/pages/about/about.jsx
+++ src/pages/settings/pages/about/about.jsx
@@ -127,7 +127,7 @@ export default function AboutPage() {
           textContent={"Oliver Koblizek"}
         />
       </div>
-      <p style={{ color: textColor }}>Version 0.0.9</p>
+      <p style={{ color: textColor }}>Version 0.1.0</p>
     </div>
   );
 }
