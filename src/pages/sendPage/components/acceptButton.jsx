2707551cfffc42c09a0e907735698bcff7d3d0e3
Blake Kaufman <blakekaufman9@gmail.com>
2026-04-29 11:42:12 -0400

    update accept button

--- src/pages/sendPage/components/acceptButton.jsx
+++ src/pages/sendPage/components/acceptButton.jsx
@@ -1,17 +1,14 @@
-import React, { useState, useMemo } from "react";
-
+import { useState, useMemo } from "react";
+import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-
-import { InputTypes } from "bitcoin-address-parser";
 import { useGlobalContextProvider } from "../../../contexts/masterInfoObject";
 import { useNodeContext } from "../../../contexts/nodeContext";
 import { useActiveCustodyAccount } from "../../../contexts/activeAccount";
+import { useOverlay } from "../../../contexts/overlayContext";
 import CustomButton from "../../../components/customButton/customButton";
-import displayCorrectDenomination from "../../../functions/displayCorrectDenomination";
-import { SMALLEST_ONCHAIN_SPARK_SEND_AMOUNT } from "../../../constants";
+import { HIDDEN_OPACITY } from "../../../constants/theme";
 
 export default function AcceptButtonSendPage({
-  canSendPayment,
   decodeSendAddress,
   errorMessageNavigation,
   btcAdress,
@@ -20,174 +17,44 @@ export default function AcceptButtonSendPage({
   paymentDescription,
   setPaymentInfo,
   setLoadingMessage,
-  navigate,
   fromPage,
-  publishMessageFunc,
-  minLNURLSatAmount,
-  maxLNURLSatAmount,
   sparkInformation,
   seletctedToken,
-  isLRC20Payment,
-
-  openOverlay,
+  useAltLayout,
+  globalContactsInformation,
+  canUseFastPay,
+  selectedPaymentMethod,
+  bitcoinBalance,
+  dollarBalanceSat,
+  isDecoding,
+  poolInfoRef,
+  swapLimits,
+  min_usd_swap_amount,
+  inputDenomination,
+  paymentValidation,
+  setDidSelectPaymentMethod,
+  conversionFiatStats,
+  primaryDisplay,
 }) {
+  const navigate = useNavigate();
+  const { openOverlay } = useOverlay();
   const { t } = useTranslation();
   const [isGeneratingInvoice, setIsGeneratingInvoice] = useState(false);
-
   const { masterInfoObject } = useGlobalContextProvider();
   const { liquidNodeInformation, fiatStats } = useNodeContext();
-
   const { currentWalletMnemoinc } = useActiveCustodyAccount();
 
-  const isLNURLAmountValid = useMemo(() => {
-    if (paymentInfo?.type !== InputTypes.LNURL_PAY) return true;
-    return (
-      convertedSendAmount >= minLNURLSatAmount &&
-      convertedSendAmount <= maxLNURLSatAmount
-    );
-  }, [
-    paymentInfo?.type,
-    convertedSendAmount,
-    minLNURLSatAmount,
-    maxLNURLSatAmount,
-  ]);
-
-  const isBitcoinAmountValid = useMemo(() => {
-    if (paymentInfo?.type !== InputTypes.BITCOIN_ADDRESS) return true;
-    return convertedSendAmount >= SMALLEST_ONCHAIN_SPARK_SEND_AMOUNT;
-  }, [paymentInfo?.type, convertedSendAmount]);
-
-  const isLRC20Valid = useMemo(() => {
-    if (!isLRC20Payment) return true;
-    console.log(
-      sparkInformation.balance >= 10,
-      seletctedToken?.balance,
-      paymentInfo?.sendAmount * 10 ** seletctedToken?.tokenMetadata?.decimals,
-      "tttt"
-    );
-    return (
-      sparkInformation.balance >= 10 &&
-      seletctedToken?.balance >=
-        paymentInfo?.sendAmount * 10 ** seletctedToken?.tokenMetadata?.decimals
-    );
-  }, [isLRC20Payment, sparkInformation?.balance, seletctedToken, paymentInfo]);
-
-  const handleBitcoinAmountError = () => {
-    openOverlay({
-      for: "error",
-      errorMessage: t("wallet.sendPages.acceptButton.onchainError", {
-        amount: displayCorrectDenomination({
-          amount: SMALLEST_ONCHAIN_SPARK_SEND_AMOUNT,
-          fiatStats,
-          masterInfoObject,
-        }),
-      }),
-    });
-  };
-
-  const handleLNURLPayError = () => {
-    const isMinError = convertedSendAmount < minLNURLSatAmount;
-    const errorAmount = isMinError ? minLNURLSatAmount : maxLNURLSatAmount;
-
-    openOverlay({
-      for: "error",
-      errorMessage: t("wallet.sendPages.acceptButton.lnurlPayError", {
-        overFlowType: isMinError ? "Minimum" : "Maximum",
-        amount: displayCorrectDenomination({
-          amount: errorAmount,
-          fiatStats,
-          masterInfoObject,
-        }),
-      }),
-    });
-  };
-
-  const handleLRC20Error = () => {
-    openOverlay({
-      for: "error",
-      errorMessage:
-        sparkInformation.balance >= 10
-          ? t("wallet.sendPages.acceptButton.balanceError")
-          : t("wallet.sendPages.acceptButton.lrc20FeeError", {
-              amount: displayCorrectDenomination({
-                amount: 10,
-                masterInfoObject,
-                fiatStats,
-              }),
-              balance: displayCorrectDenomination({
-                amount: sparkInformation.balance,
-                masterInfoObject,
-                fiatStats,
-              }),
-            }),
-    });
-  };
-
-  const handleInsufficientBalanceError = () => {
-    openOverlay({
-      for: "error",
-      errorMessage: t("wallet.sendPages.acceptButton.noSendAmountError"),
-    });
-  };
-
-  const handleNoSendAmountError = () => {
-    openOverlay({
-      for: "error",
-      errorMessage: t("wallet.sendPages.acceptButton.balanceError"),
-    });
-  };
-
-  const validatePaymentAmount = () => {
-    console.log("validating payment amount");
-    console.log(paymentInfo, "payment info");
-    console.log(convertedSendAmount, "converted send amount");
-    console.log(canSendPayment, "can send payment");
-    console.log(!isBitcoinAmountValid);
-    console.log(
-      paymentInfo?.type === InputTypes.LNURL_PAY && !isLNURLAmountValid
-    );
-    console.log(!isLRC20Valid);
-    console.log(!canSendPayment && !!paymentInfo?.sendAmount);
-    console.log(isLRC20Payment);
-    if (!paymentInfo?.sendAmount) {
-      handleNoSendAmountError();
-      return false;
-    }
-
-    // if (!isLiquidAmountValid) {
-    //   handleLiquidAmountError();
-    //   return false;
-    // }
-
-    if (!isBitcoinAmountValid) {
-      handleBitcoinAmountError();
-      return false;
-    }
-
-    if (paymentInfo?.type === InputTypes.LNURL_PAY && !isLNURLAmountValid) {
-      handleLNURLPayError();
-      return false;
-    }
-
-    if (!isLRC20Valid) {
-      handleLRC20Error();
-      return false;
-    }
-
-    if (!canSendPayment && !!paymentInfo?.sendAmount) {
-      handleInsufficientBalanceError();
-      return false;
-    }
-
-    return true;
-  };
-
   const handleEnterSendAmount = async () => {
-    if (!validatePaymentAmount()) {
+    if (!paymentValidation.isValid) {
+      const errorMessage = paymentValidation.getErrorMessage(
+        paymentValidation.primaryError,
+      );
+      openOverlay({ for: "error", errorMessage });
       return;
     }
 
     setIsGeneratingInvoice(true);
+    setDidSelectPaymentMethod(true);
 
     try {
       await decodeSendAddress({
@@ -196,25 +63,41 @@ export default function AcceptButtonSendPage({
         goBackFunction: errorMessageNavigation,
         setPaymentInfo,
         liquidNodeInformation,
-        masterInfoObject,
+        masterInfoObject: {
+          ...masterInfoObject,
+          userBalanceDenomination: inputDenomination,
+        },
         navigate,
-        // maxZeroConf:
-        //   minMaxLiquidSwapAmounts?.submarineSwapStats?.limits?.maximalZeroConf,
         comingFromAccept: true,
         enteredPaymentInfo: {
           amount: convertedSendAmount,
           description: paymentDescription,
+          lnInvoiceData: paymentInfo?.data?.invoice
+            ? {
+                pr: paymentInfo.data.invoice,
+                successAction: paymentInfo.data.successAction,
+              }
+            : null,
         },
         paymentInfo,
         setLoadingMessage,
         parsedInvoice: paymentInfo.decodedInput,
         fromPage,
-        publishMessageFunc,
-        // webViewRef,
         sparkInformation,
         seletctedToken,
         currentWalletMnemoinc,
         t,
+        sendWebViewRequest: null,
+        globalContactsInformation,
+        usablePaymentMethod: selectedPaymentMethod || "BTC",
+        bitcoinBalance,
+        dollarBalanceSat,
+        convertedSendAmount,
+        poolInfoRef,
+        swapLimits,
+        min_usd_swap_amount,
+        conversionFiatStats,
+        primaryDisplay,
       });
     } catch (error) {
       console.log("Accept button error:", error);
@@ -223,12 +106,22 @@ export default function AcceptButtonSendPage({
     }
   };
 
+  const memorizedStyles = useMemo(
+    () => ({
+      opacity: paymentValidation.isValid ? 1 : HIDDEN_OPACITY,
+      width: "max-content",
+    }),
+    [paymentValidation],
+  );
+
   return (
     <CustomButton
-      buttonStyles={{ marginTop: "0" }}
-      useLoading={isGeneratingInvoice}
+      buttonStyles={memorizedStyles}
+      useLoading={isGeneratingInvoice || isDecoding}
       actionFunction={handleEnterSendAmount}
-      textContent={t("constants.accept")}
+      textContent={
+        canUseFastPay ? t("constants.confirm") : t("constants.review")
+      }
     />
   );
 }
