6f4bef1bdc53777bec58ccc40956b179ccdde53d
Blake Kaufman <blakekaufman9@gmail.com>
2026-04-29 11:49:44 -0400

    updating send page number input

--- src/pages/sendPage/components/numberInput.jsx
+++ src/pages/sendPage/components/numberInput.jsx
@@ -1,36 +1,83 @@
-import { useEffect, useState } from "react";
-
+import { useCallback, useEffect, useState } from "react";
 import CustomNumberKeyboard from "../../../components/customNumberKeyboard/customNumberKeyboard";
-import { useGlobalContextProvider } from "../../../contexts/masterInfoObject";
 
 export default function NumberInputSendPage({
   setPaymentInfo,
   paymentInfo,
   fiatStats,
+  selectedLRC20Asset,
+  seletctedToken,
+  inputDenomination,
+  primaryDisplay,
 }) {
-  console.log(paymentInfo, "y");
-  const { masterInfoObject } = useGlobalContextProvider();
-  const [amount, setAmount] = useState(paymentInfo?.sendAmount);
-  console.log(amount, "heysasdkfknsd");
-  useEffect(() => {
-    setPaymentInfo((prev) => {
-      return { ...prev, sendAmount: amount };
-    });
-  }, [amount]);
+  const decimals = seletctedToken?.tokenMetadata?.decimals;
+  const amount = paymentInfo?.sendAmount;
 
-  // Effect to update amount when paymentInfo.sendAmount changes
-  useEffect(() => {
-    if (paymentInfo?.sendAmount !== amount) {
-      setAmount(paymentInfo.sendAmount);
-    }
-  }, [paymentInfo?.sendAmount]);
+  const handleSetAmount = useCallback(
+    (newAmountOrUpdater) => {
+      setPaymentInfo((prev) => {
+        const newAmount =
+          typeof newAmountOrUpdater === "function"
+            ? newAmountOrUpdater(prev.sendAmount)
+            : newAmountOrUpdater;
+        return {
+          ...prev,
+          sendAmount: newAmount,
+          feeQuote: undefined,
+          paymentFee: 0,
+          supportFee: 0,
+        };
+      });
+    },
+    [setPaymentInfo],
+  );
+
+  const lrc20InputFunction = useCallback(
+    (input) => {
+      if (input === null) {
+        const newAmount = String(amount).slice(0, -1);
+        handleSetAmount(newAmount);
+      } else {
+        let newNumber = "";
+        if (amount?.includes(".") && input === ".") {
+          newNumber = amount;
+        } else if (
+          amount?.includes(".") &&
+          amount.split(".")[1].length >= decimals
+        ) {
+          newNumber = amount;
+        } else {
+          newNumber = String(amount) + input;
+        }
+
+        // Add leading 0 if starting with decimal point
+        if (newNumber.startsWith(".")) {
+          newNumber = "0" + newNumber;
+        }
+
+        // Remove leading zeros before digits
+        newNumber = newNumber.replace(/^(-?)0+(?=\d)/, "$1");
+
+        handleSetAmount(newNumber);
+      }
+    },
+    [amount, decimals, handleSetAmount],
+  );
 
   return (
     <CustomNumberKeyboard
-      showDot={masterInfoObject.userBalanceDenomination === "fiat"}
-      keyboardContianerClassName={"custon-keyboard-styles"}
-      setAmountValue={setAmount}
-      amount={amount}
+      showDot={
+        (inputDenomination === "fiat" && selectedLRC20Asset === "Bitcoin") ||
+        selectedLRC20Asset !== "Bitcoin" ||
+        primaryDisplay.denomination === "fiat"
+      }
+      setAmountValue={handleSetAmount}
+      usingForBalance={true}
+      fiatStats={fiatStats}
+      useMaxBalance={selectedLRC20Asset === "Bitcoin"}
+      customFunction={
+        selectedLRC20Asset !== "Bitcoin" ? lrc20InputFunction : null
+      }
     />
   );
 }
